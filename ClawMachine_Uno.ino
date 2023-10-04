e73dc020c8f84ae475014ec755e2a9c0a09f7874
Krisztián József Varga <aradorasxeon@gmail.com>
2023-10-04 19:09:35 +0200

    implemented own delay since it was skipped

--- ClawMachine_Uno.ino
+++ ClawMachine_Uno.ino
@@ -16,14 +16,45 @@
 
 
 
-const int TIME = 1500;
+const int TIME = 500;
 const int MICROTIME = 1500; //suggestion in the video: 700-3000
-const int stepCount = 100; //100 step is about 1/4 revolution, at least it seems that way
+const int stepCount = 1000; //100 step is about 1/4 revolution, at least it seems that way
+
+void myDelay(unsigned long ms)
+{
+  unsigned long startTime = millis();
+  unsigned long deltaTime = 0;
+  unsigned long currentTime = 0;
+ 
+  CHECK_AGAIN:
+  currentTime = millis();
+  if(startTime > currentTime) //in case myDelay was called near the end of its max capacity
+  {
+    return; //we just simply return
+  }
+  deltaTime = currentTime - startTime;
+
+  if(deltaTime > ms)
+  {
+    return;
+  }
+  else
+  {
+    goto CHECK_AGAIN;
+  }
+
+}
+
+
+unsigned long currentTime()
+{
+  return millis();
+}
 
 
 void setup() 
 {
-  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ENABLER, OUTPUT);
 
@@ -40,16 +71,19 @@ void setup()
   pinMode(LIMIT_Z, INPUT);
 
   digitalWrite(ENABLER, LOW);
+  Serial.write("SETUP");
 
 }
 
 void loop() 
 {
+  Serial.write("A");
 
 //Test mode:
   digitalWrite(STEP_DIR_X, HIGH);
   digitalWrite(STEP_DIR_Y, HIGH);                    
   digitalWrite(STEP_DIR_Z, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
 
   //Move all 3 at once one revolution
   for(int i = 0; i<stepCount; i++)
@@ -57,39 +91,40 @@ void loop()
     digitalWrite(STEP_PIN_X, HIGH);
     digitalWrite(STEP_PIN_Y, HIGH);
     digitalWrite(STEP_PIN_Z, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
-
     delayMicroseconds(MICROTIME);
     digitalWrite(STEP_PIN_X, LOW);
     digitalWrite(STEP_PIN_Y, LOW);
     digitalWrite(STEP_PIN_Z, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
-
     delayMicroseconds(MICROTIME);
   }
+  Serial.println("B");
+  Serial.println(millis());
+  myDelay(TIME);
+  Serial.println(millis());
 
-
-  delay(TIME);
-
+  Serial.write("C");
   digitalWrite(STEP_DIR_X, LOW);
   digitalWrite(STEP_DIR_Y, LOW);                    
-  digitalWrite(STEP_DIR_Z, LOW);  
+  digitalWrite(STEP_DIR_Z, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+
 
   for(int i = 0; i<stepCount; i++)
   {
     digitalWrite(STEP_PIN_X, HIGH);
     digitalWrite(STEP_PIN_Y, HIGH);
     digitalWrite(STEP_PIN_Z, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
-
     delayMicroseconds(MICROTIME);
     digitalWrite(STEP_PIN_X, LOW);
     digitalWrite(STEP_PIN_Y, LOW);
     digitalWrite(STEP_PIN_Z, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
-
     delayMicroseconds(MICROTIME);
   }   
+
+  Serial.println("D");
+  myDelay(TIME);
+  Serial.println(millis());
+
 
 }
 
